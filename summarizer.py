95efa9f333560492779646df7016f4c9e6957907
Nanda Kishore <229775828+NandaKishore166@users.noreply.github.com.>
2026-04-09 18:47:28 +0530

    Removed transformers dependency completely

--- summarizer.py
+++ summarizer.py
@@ -1,11 +1,7 @@
-from transformers import pipeline
-
-summarizer = pipeline("text-generation", model="gpt2")
-
 def generate_summary(text):
     if not text:
         return "No important logs found."
-    
+
     # simple fast summary
     sentences = text.split(".")
     return " | ".join(sentences[:2])
