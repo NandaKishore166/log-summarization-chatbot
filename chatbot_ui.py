b1028a92bf124464549726f99f55bbec43b54c68
NandaKishore166 <229775828+NandaKishore166@users.noreply.github.com>
2026-04-09 17:10:42 +0530

    Update Flask app to use environment port

--- chatbot_ui.py
+++ chatbot_ui.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from preprocess import clean_logs
 from extractor import extract_events
 from summarizer import generate_summary
-
+import os
 app = Flask(__name__)
 
 with open("logs.txt") as f:
@@ -40,4 +40,5 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
